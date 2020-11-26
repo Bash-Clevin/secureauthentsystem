fa391e510e74c927e17789ae4f6ba814c69b99b7
Bash-clevin <clevinbash@gmail.com>
2020-11-26 16:23:57 +0300

    final changes to db and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+#SECUREAUTHENTICATION SYSTEM USING QR AND OTP
+copy the files to youtr htdocs directory in xampp 
+create a DB in php my admin and import the database in assets folder
+login as admin with admin@admin.com password is 123456
--- assets/secauthsys.sql
+++ assets/secauthsys.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2019 at 06:39 AM
--- Server version: 10.1.22-MariaDB
--- PHP Version: 7.1.4
+-- Generation Time: Nov 26, 2020 at 02:21 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,7 +34,7 @@ CREATE TABLE `clients` (
   `email` varchar(60) NOT NULL,
   `passwd` varchar(70) NOT NULL,
   `role` varchar(10) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT 0,
   `hash` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -44,7 +43,6 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id`, `fname`, `sname`, `email`, `passwd`, `role`, `status`, `hash`) VALUES
-(6, 'kevin', 'clinton', 'kevinclint06@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'level1', 0, ''),
 (9, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 'level1', 0, '');
 
 -- --------------------------------------------------------
@@ -63,7 +61,7 @@ CREATE TABLE `root` (
 --
 
 INSERT INTO `root` (`email`, `pass`) VALUES
-('kevinclint06@gmail.com', '123456');
+('admin@admin.com', '123456');
 
 --
 -- Indexes for dumped tables
@@ -90,7 +88,8 @@ ALTER TABLE `root`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
